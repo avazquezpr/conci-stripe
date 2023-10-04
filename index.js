07c1acd261807cc649fa81b07b434861e90c8c2d
tekangel <138501647+tekangel@users.noreply.github.com>
2023-10-04 16:32:57 -0400

    Secret key show

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY || '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');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.post('/create-payment-intent', async (req, res) => {
     }
 
     const correctedAmount = amount * 100; // Stripe expects the amount in cents
-
+    console.log(process.env.STRIPE_SECRET_KEY)
     try {
         const paymentIntent = await stripe.paymentIntents.create({
             correctedAmount,  // using the amount from the request body
